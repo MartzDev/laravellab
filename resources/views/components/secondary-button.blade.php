365e060c3cac96f9ac7c013db281339d35007211
MartzDev <155332741+MartzDev@users.noreply.github.com>
2025-03-02 18:14:13 -0500

    actualiacion boton secundarip

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,5 +1,5 @@
 <button
-    {{ $attributes->merge(['type' => 'button', 'class' => 'rounded-md bg-white dark:bg-white/10 px-3.5 py-2.5 shadow-xs text-sm font-semibold text-gray-900 border border-gray-300 dark:border-black dark:text-white hover:bg-white/20 dark:hover:bg-white/20 active:bg-white dark:active:bg-white/10 focus:outline-hidden focus:ring-2 focus:ring-indigo-500 dark:focus:ring-gray-500 focus:ring-offset-2 dark:focus:ring-offset-black disabled:opacity-50 transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['type' => 'button', 'class' => 'rounded-md bg-white dark:bg-white/10 px-3.5 py-2.5 shadow-xs text-sm font-semibold text-gray-900 border border-gray-300 dark:border-black dark:text-white hover:bg-white/20 dark:hover:bg-white/20 active:bg-white dark:active:bg-white/10 focus:outline-hidden focus:ring-2 focus:ring-gray-500 dark:focus:ring-gray-500 focus:ring-offset-2 dark:focus:ring-offset-black disabled:opacity-50 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
 
